06c4e348ad9ced9bee76de6ab558faab8ddce694
ABADIE Loïc <loic.abadie@pictime.com>
2017-10-03 14:49:45 +0200

    [#Pa03] - header version

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import Response
+
 app = Flask(__name__)
+version = "0.0.1"
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    resp = Response("")
+    resp.headers['Version'] = version
+    return resp
